91b1c1dd7410b1f167ee2d4d4198d4726f3032b2
Mohamed-Hmidouch <ffa54202@gmail.com>
2025-01-13 22:26:09 +0100

    push the craete

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'youcode/you-demy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b9734a94adb980455ba98581ae564a6466d66d21',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'youcode/you-demy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b9734a94adb980455ba98581ae564a6466d66d21',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
