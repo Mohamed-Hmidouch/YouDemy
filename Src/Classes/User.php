692ac174c488a4c6a678d9ed5602c76f67257687
Mohamed-Hmidouch <ffa54202@gmail.com>
2025-01-14 15:49:43 +0100

    login function

---
+++ Src/Classes/User.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Classes;
+require_once _DIR_ . "/../../vendor/autoload.php";
+
+class User
+{
+    private $id;
+    private $name;
+    private $email;
+
+    private $password;
+    private $role;
+    public function __construct($id, $name, $email, $password, $role){
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+    }
+    public function getRole(){
+        return $this->role;
+    }
+    public function getId(){
+        return $this->id;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function getEmail(){
+        return $this->email;
+    }
+    public function getPassword(){
+        return $this->password;
+    }
+
+}
--- Src/Models/Auth/AuthModel.php
+++ Src/Models/Auth/AuthModel.php
@@ -2,7 +2,34 @@
 
 namespace App\Models\Auth;
 
+use App\Config\Database;
+
+use App\Classes\User;
+
+Use Pdo;
 class AuthModel
 {
-
+    private $connection;
+public function __construct(){
+    $this->connection = Database::getConnection();
+}
+    public function findUser($email, $password){
+        $query = "SELECT * FROM Users WHERE email = :email;
+";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$row) {
+            return null;
+        }
+        if($row['password'] == $password) {
+            return new User($row['id'], $row['nom'], $row['email'], $row['password'],$row['role']);
+        }else{
+            return null;
+        }
+//        if ($row && password_verify($password, $row["password"])) {
+//            return new User($row['id'], $row["name"],$row["email"], $row["password"], $row["role"]);
+//        }
+    }
 }
